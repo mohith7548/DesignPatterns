e1bafc1694998722703d05a9bca6dfd8b42c8f8a
Mohith <mohith7548@gmail.com>
2019-08-28 09:54:43 +0530

    Builder Pattern Done!

---
+++ BuilderPattern/src/HouseBuilder.java
@@ -0,0 +1,13 @@
+interface HouseBuilder
+{
+
+    public void buildBasement();
+
+    public void buildStructure();
+
+    public void bulidRoof();
+
+    public void buildInterior();
+
+    public House getHouse();
+} 
